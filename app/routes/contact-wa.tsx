fcb5e520bc6b6009dd8e5cfac06198c510511069
Damha <sofyandamha@gmail.com>
2026-03-12 14:58:43 +0700

    cta ganti nomot

--- app/routes/contact-wa.tsx
+++ app/routes/contact-wa.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect } from 'react';
 import { createMetaFunction, seoData } from "~/lib/meta";
-const WHATSAPP_URL = 'https://wa.me/6289683711115?text=Halo,%20saya%20melihat%20informasi%20ini%20di%20website%20dan%20ingin%20mengetahui%20detail%20solusinya.%20Terima%20kasih';
+const WHATSAPP_URL = 'https://wa.me/6281199895152?text=Halo,%20saya%20melihat%20informasi%20ini%20di%20website%20dan%20ingin%20mengetahui%20detail%20solusinya.%20Terima%20kasih';
 
 export const meta = createMetaFunction(seoData.contact);
 
